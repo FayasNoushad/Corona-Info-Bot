8a791e312afb9236278fa59ed1d24e370bd5f3d0
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-21 12:13:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,4 +8,27 @@ FayasNoushad = Client(
     api_hash = os.environ["API_HASH"]
 )
 
+API = "https://api.sumanjay.cf/covid/covid/?country="
+
+@FayasNoushad.on_message(filters.private & filters.text)
+async def covid_info(bot, update):
+    country = update.text.replace(" ", "+")
+    info = requests.get(API + country).json()
+    covid_info = f"""
+--**Covid 19 Information**--
+
+Country : {info['country']}
+Actived : {info['active']}
+Confirmed : {info['confirmed']}
+Deaths : {info['deaths']}
+Id : {info['id']}
+Last Update : {info['last_update']}
+Latitude : {info['latitude']}
+Longitude : {info['longitude']}
+Recovered : {info['recovered']}
+
+Made by @FayasNoushad
+"""
+    await update.reply_text(covid_info)
+
 FayasNoushad.run()

d320376f9351d82832c31ed2141facc124482f53
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-21 12:50:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,18 +13,20 @@ API = "https://api.sumanjay.cf/covid/covid/?country="
 
 @FayasNoushad.on_message(filters.private & filters.text)
 async def covid_info(bot, update):
-    country = update.text.replace(" ", "+")
-    info = requests.get(f'{API}{country}/').json()
-    country = info['country']
-    active = info['active']
-    confirmed = info['confirmed']
-    deaths = info['deaths']
-    info_id = info['id']
-    last_update = info['last_update']
-    latitude = info['latitude']
-    longitude = info['longitude']
-    recovered = info['recovered']
-    covid_info = f"""
+    try:
+        country = update.text.replace(" ", "+")
+        r = requests.get(API + country)
+        info = r.json()
+        country = info['country']
+        active = info['active']
+        confirmed = info['confirmed']
+        deaths = info['deaths']
+        info_id = info['id']
+        last_update = info['last_update']
+        latitude = info['latitude']
+        longitude = info['longitude']
+        recovered = info['recovered']
+        covid_info = f"""
 --**Covid 19 Information**--
 
 Country : {country}
@@ -39,6 +41,8 @@ Recovered : {recovered}
 
 Made by @FayasNoushad
 """
-    await update.reply_text(covid_info)
+        await update.reply_text(covid_info)
+    except Exception as error:
+        await update.reply_text(error)
 
 FayasNoushad.run()

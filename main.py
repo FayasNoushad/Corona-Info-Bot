c2ec0cb77da1a7abe6cbec16953ddddde2985fee
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-07-27 10:09:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ async def start(bot, update):
 
 @FayasNoushad.on_message(filters.private & filters.text)
 async def reply_info(bot, update):
-    country = update.text.replace(" ", "+")
+    country = update.text.replace(" ", "+").lower()
     reply_markup = BUTTONS
     await update.reply_text(
         text=covid_info(country),
